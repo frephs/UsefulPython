c6fa7cd4976d5d27fe7fe0734affb8ad674412fa
Francesco <ffrancescogenovese@gmail.com>
2021-10-14 18:25:52 +0200

    Commented the colorCorrectPdf script

--- colorCorrectPdfs.py
+++ colorCorrectPdfs.py
@@ -15,6 +15,8 @@ def getPDFs():
             list_pdf.remove(item)
     return list_pdf
 
+
+#converting pdfs to images to edit them
 def pdfToImg():
     list_pdf = getPDFs()
     for pdf in list_pdf:
@@ -23,6 +25,7 @@ def pdfToImg():
             images[i].save('prov/'+str(pdf)+'page'+ str(i) +'.png', 'PNG')
             print( 'OK prov/'+str(pdf)+'page'+ str(i) +'.png')
 
+#edfiting an image colors
 def changeColor(image_file):
     print(image_file)
     im = Image.open(image_file)
@@ -31,21 +34,22 @@ def changeColor(image_file):
     r1, g1, b1 = 170, 170, 170 # Original value
     r2, g2, b2 = 41, 51, 45 # Value that we want to replace it with
     red, green, blue = data[:,:,0], data[:,:,1], data[:,:,2]
-    mask = (red > r1) & (green > g1) & (blue > b1 )
+    mask = (red > r1) & (green > g1) & (blue > b1 ) #replacing the values if they are they are next to white
     data[:,:,:3][mask] = [r2, g2, b2]
 
     r1, g1, b1 = 51, 51, 51 # Original value
     r2, g2, b2 = 255, 255, 255 # Value that we want to replace it with
     red, green, blue = data[:,:,0], data[:,:,1], data[:,:,2]
-    mask = (red == r1) & (green == g1) & (blue == b1)
+    mask = (red == r1) & (green == g1) & (blue == b1) #replacing a dark background with a white one
     data[:,:,:3][mask] = [r2, g2, b2]
-
+    #saving
     image_file = image_file.replace(".png","").replace("prov/","")
     im = Image.fromarray(data)
     im.save(f'final/{image_file}.png')
     print("Done")
     return f"Done {image_file}"
 
+#colorCorrecting each page of the pdf
 def modifyPages():
     list_png = os.listdir("prov")
     for item in list_png:
@@ -57,10 +61,11 @@ def modifyPages():
     for image in list_png:
         changeColor("prov/"+image)
 
-
+# 3,2,1 GO!
 if __name__ == '__main__':
     try:
         pdfToImg()
     except Exception as e:
         pass
     modifyPages()
+    #you can now select your images and print them as a pdf for yourself thank you
